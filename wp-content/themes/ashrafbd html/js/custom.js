8f14bd2696ea459ed5587601a1ff260e4486604f
ashrafbd1496 <asraf1496@gmail.com>
2022-06-13 12:34:38 +0600

    added underscore theme

---
+++ wp-content/themes/ashrafbd html/js/custom.js
@@ -0,0 +1,198 @@
+(function($) {
+
+	"use strict";
+
+	/* ----------------------------------------------------------- */
+	/*  FUNCTION TO STOP LOCAL AND YOUTUBE VIDEOS IN SLIDESHOW
+    /* ----------------------------------------------------------- */
+
+	function stop_videos() {
+		var video = document.getElementById("video");
+		if (video.paused !== true && video.ended !== true) {
+			video.pause();
+		}
+		$('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+	}
+
+	$(document).ready(function() {
+
+		/* ----------------------------------------------------------- */
+		/*  STOP VIDEOS
+        /* ----------------------------------------------------------- */
+
+		$('.slideshow nav span').on('click', function () {
+			stop_videos();
+		});
+
+		/* ----------------------------------------------------------- */
+		/*  FIX REVEALATOR ISSUE AFTER PAGE LOADED
+        /* ----------------------------------------------------------- */
+
+		$(".revealator-delay1").addClass('no-transform');
+
+		/* ----------------------------------------------------------- */
+		/*  PORTFOLIO GALLERY
+        /* ----------------------------------------------------------- */
+
+		if ($('.grid').length) {
+			new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
+		}
+
+		/* ----------------------------------------------------------- */
+		/*  BUTTONS ANIMATION
+        /* ----------------------------------------------------------- */
+		function checkSize() {
+			if ($( document ).width() > 992) {
+				var btn_hover = "";
+				$(".btn").each(function() {
+					var btn_text = $(this).text();
+					$(this).addClass(btn_hover).empty().append("<span data-hover='" + btn_text + "'>" + btn_text + "</span>");
+				});
+			}
+		}
+		checkSize();
+		window.addEventListener('resize', function () {
+			checkSize();
+		});
+
+		/* ----------------------------------------------------------- */
+		/*  HIDE HEADER WHEN PORTFOLIO SLIDESHOW OPENED
+        /* ----------------------------------------------------------- */
+
+		$(".grid figure").on('click', function() {
+			$("#navbar-collapse-toggle").addClass('hide-header');
+		});
+
+		/* ----------------------------------------------------------- */
+		/*  SHOW HEADER WHEN PORTFOLIO SLIDESHOW CLOSED
+        /* ----------------------------------------------------------- */
+
+		$(".nav-close").on('click', function() {
+			$("#navbar-collapse-toggle").removeClass('hide-header');
+		});
+		$(".nav-prev").on('click', function() {
+			if ($('.slideshow ul li:first-child').hasClass('current')) {
+				$("#navbar-collapse-toggle").removeClass('hide-header');
+			}
+		});
+		$(".nav-next").on('click', function() {
+			if ($('.slideshow ul li:last-child').hasClass('current')) {
+				$("#navbar-collapse-toggle").removeClass('hide-header');
+			}
+		});
+
+		/* ----------------------------------------------------------- */
+		/*  PORTFOLIO DIRECTION AWARE HOVER EFFECT
+        /* ----------------------------------------------------------- */
+
+		var item = $(".grid li figure");
+		var elementsLength = item.length;
+		for (var i = 0; i < elementsLength; i++) {
+			$(item[i]).hoverdir();
+		}
+
+	
+		/* ----------------------------------------------------------- */
+		/*  AJAX CONTACT FORM
+        /* ----------------------------------------------------------- */
+
+		$(".contactform").on("submit", function() {
+			$(".output_message").text("Sending...");
+
+			var form = $(this);
+			$.ajax({
+				url: form.attr("action"),
+				method: form.attr("method"),
+				data: form.serialize(),
+				success: function(result) {
+					if (result == "success") {
+						$(".form-inputs").css("display", "none");
+						$(".box p").css("display", "none");
+						$(".contactform").find(".output_message").addClass("success");
+						$(".output_message").text("Message Sent!");
+					} else {
+						$(".tabs-container").css("height", "440px");
+
+						$(".contactform").find(".output_message").addClass("error");
+						$(".output_message").text("Error Sending!");
+					}
+				}
+			});
+
+			return false;
+		});
+
+	});
+
+	$(document).keyup(function(e) {
+
+		/* ----------------------------------------------------------- */
+		/*  KEYBOARD NAVIGATION IN PORTFOLIO SLIDESHOW
+        /* ----------------------------------------------------------- */
+		if (e.keyCode === 27) {
+			stop_videos();
+			$('.close-content').click();
+			$("#navbar-collapse-toggle").removeClass('hide-header');
+		}
+		if ((e.keyCode === 37) || (e.keyCode === 39)) {
+			stop_videos();
+		}
+	});
+
+	/* ------------------------------js for load more button----------------------------- */
+	// $(function(){
+	// 	$(".lm").slice(0, 6).show(); // select the first ten
+	// 	$("#load").click(function(e){ // click event for load more
+	// 		e.preventDefault();
+	// 		$(".lm:hidden").slice(0, 6).show(); // select next 10 hidden divs and show them
+	// 		if($(".lm:hidden").length == 0){ // check if any hidden divs still exist
+	// 			alert("No more items"); // alert if there are none left
+	// 		}
+	// 	});
+	// });
+
+
+	// $j(document).ready(function () {
+    //     size_li = $j(".grid li").size();
+    //     x=9;
+    //     $j('.grid li:lt('+x+')').show();
+    //     $j('#load').click(function () {
+    //         x= (x+3 <= size_li) ? x+3 : size_li;
+    //         $j('.grid li:lt('+x+')').show();
+    //     });
+    // });
+
+
+	// $( document ).ready(function () {
+	// 	$(".lm").slice(0, 3).show();
+	// 	  if ($(".lm:hidden").length != 0) {
+	// 		$("#load").show();
+	// 	  }   
+	// 	  $("#load").on('click', function (e) {
+	// 		e.preventDefault();
+	// 		$(".lm:hidden").slice(0, 6).slideDown();
+	// 		if ($(".lm:hidden").length == 0) {
+	// 		  $("#load").fadeOut('slow');
+	// 		}
+	// 	  });
+	// 	});
+	
+		// Code for validating the form
+	// Visit http://www.javascript-coder.com/html-form/javascript-form-validation.phtml
+	// for details
+	var frmvalidator  = new Validator("myemailform");
+	frmvalidator.addValidation("name","req","Please provide your name"); 
+	frmvalidator.addValidation("email","req","Please provide your email"); 
+	frmvalidator.addValidation("email","email","Please enter a valid email address"); 
+
+
+
+
+
+
+
+
+
+
+
+})(jQuery);
